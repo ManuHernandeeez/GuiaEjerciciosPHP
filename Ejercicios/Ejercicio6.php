7eb23d2491b6813cfda12db23c48564644514844
Manuel Hernandez <manu.hernandez.15.2002@gmail.com>
2025-07-16 20:08:20 -0600

    Imprime como arreglo

--- Ejercicios/Ejercicio6.php
+++ Ejercicios/Ejercicio6.php
@@ -1,1 +1,25 @@
-#Ejercicio De HakerRank
+<?php
+function fizzBuzz($n) {
+    $resultado = [];
+    for ($i = 1; $i <= $n; $i++) {
+        if ($i % 3 == 0 && $i % 5 == 0) {
+            $resultado[] = "FizzBuzz";
+        } elseif ($i % 3 == 0) {
+            $resultado[] = "Fizz";
+        } elseif ($i % 5 == 0) {
+            $resultado[] = "Buzz";
+        } else {
+            $resultado[] = (string)$i; 
+        }
+    }
+    return $resultado;
+}
+
+$n = 100;
+$respuesta = fizzBuzz($n);
+
+// Mostrar el resultado
+echo "<pre>";
+print_r($respuesta);
+echo "</pre>";
+?>
