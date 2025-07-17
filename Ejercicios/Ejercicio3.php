e9d83e55f2c666552df3de45269c8c766c8c4b23
Daniel Alas <alasalexander76@gmail.com>
2025-07-16 19:14:44 -0600

    Ejercicio 3 agregado

--- Ejercicios/Ejercicio3.php
+++ Ejercicios/Ejercicio3.php
@@ -1,1 +1,37 @@
-Daniel
+<?php 
+
+function esPalindromo($texto) 
+{
+    $limpio = strtolower(str_replace(' ', '', $texto)); //limpiar los espacios 
+
+    $invertido = strrev($limpio);
+
+    return $limpio === $invertido;
+}
+
+$frase = "reconocer";
+$resultado = esPalindromo($frase) ? "Es un palíndromo" : "No es un palíndromo";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EJERCICIO 3 - Palíndromos</title>
+</head>
+<body>
+
+    <h2>Verificador de Palíndromos</h2>
+    <p>
+        <strong>Frase Evaluada: </strong>
+        <?= $frase ?>
+    </p>
+    <p>
+        <strong>Resultado: </strong>
+        <?= $resultado ?>
+    </p>
+    
+</body>
+</html>
